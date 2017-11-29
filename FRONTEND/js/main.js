b2158b52df7aab4adb418787cfebc2d0b96c5795
Mikael Ahlström <mikael.ahlstrom@metropolia.fi>
2017-11-29 15:47:59 +0200

    upload dropmenu works

--- FRONTEND/js/main.js
+++ FRONTEND/js/main.js
@@ -1,52 +1,57 @@
 // JavaScript Document
 /*jshint esversion: 6 */
-/*
-// Select buttons
+
 const hotTab = document.querySelector('#hot_tab');
 const freshTab = document.querySelector('#fresh_tab');
-
-// Create eventListeners
-function changeTab() {
-	'use strict';
-	hotTab.classList.toggle('active');
-	freshTab.classList.toggle('active');
-	}
-*/
 const openMenu = document.querySelector('#open_menu');
 const closeMenu = document.querySelector('#close_menu');
 const openUpload = document.querySelector('#open_upload');
 const closeUpload = document.querySelector('#close_upload');
+const myNav = document.querySelector('#myNav');
+const upload = document.querySelector('#upload');
 
-
-function openNav() {
+const changeTab = () => {
 	'use strict';
-    document.getElementById("myNav").style.height = "100%";
-	openUpload.classList.toggle('visibility_hidden');
+	hotTab.classList.toggle('active');
+	freshTab.classList.toggle('active');
+}
+
+
+const openNav = () => {
+	'use strict';
+	myNav.style.height = "100%";
+	// openUpload.classList.toggle('visibility_hidden');
 	closeMenu.classList.toggle('display_none');
 	openMenu.classList.toggle('display_none');
 }
 
-function closeNav() {
+const closeNav = () => {
 	'use strict';
-    document.getElementById("myNav").style.height = "0%";
-	openUpload.classList.toggle('visibility_hidden');
+	myNav.style.height = "0%";
+	// openUpload.classList.toggle('visibility_hidden');
 	closeMenu.classList.toggle('display_none');
 	openMenu.classList.toggle('display_none');
 }
 
-function openUpload() {
+
+
+const openUploadi =()=> {
 	'use strict';
-    document.getElementById("upload").style.height = "100%";
+    upload.style.height = "100%";
 	openMenu.classList.toggle('visibility_hidden');
 	openUpload.classList.toggle('display_none');
 	closeUpload.classList.toggle('display_none');
 }
 
 
-function closeUpload() {
+
+
+
+const closeUploadi =()=> {
 	'use strict';
-    document.getElementById("upload").style.height = "0%";
+    upload.style.height = "0%";
 	openMenu.classList.toggle('visibility_hidden');
 	openUpload.classList.toggle('display_none');
 	closeUpload.classList.toggle('display_none');
 }
+
