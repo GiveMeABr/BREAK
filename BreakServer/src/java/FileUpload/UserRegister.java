e02880a5995212aa17100d394e2fa28c832ff321
anttinym <nyman.antti@gmail.com>
2017-12-07 10:47:57 +0200

    user register working

--- BreakServer/src/java/FileUpload/UserRegister.java
+++ BreakServer/src/java/FileUpload/UserRegister.java
@@ -15,7 +15,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.ws.rs.FormParam;
 import model.Users;
 
 /**
