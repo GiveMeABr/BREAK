661c07fd35de8b2381bd7573f0f6c7f1a3be2f21
Mikael Ahlström <mikael.ahlstrom@metropolia.fi>
2017-12-08 19:10:54 +0200

    Attempt to fetch from server

--- FRONTEND/js/markup.js
+++ FRONTEND/js/markup.js
@@ -1,21 +1,9 @@
 'use strict';
 /*jshint esversion: 6 */
 
-const article = {
-	img: 'img/daniel-monteiro-313651.jpg',
-	text: 'Testi onnistui',
-	title: 'Testi onnistui',
-	user: 'img/bekir-donmez-372498.jpg',
-	votes: '+ 420'
-};
-
-const comment = {
-	user: '',
-	profilePic: '',
-	commentText: '',
-	date: ''
-};
-
+let article;
+let comment;
+/*
 const markupImg = `
   <!-- MEDIA ARTICLE -->
   <article>
@@ -30,7 +18,7 @@ const markupImg = `
       </header>
       <h2>${article.votes}</h2>
     </div>
-    <div class="article_media"> <img src="${article.img}" class="article">
+    <div class="article_media"> <img src="${article.article}" class="article">
     </div>
     <div class="buttons"> <button class="upvote"><img src="icons/upvote.png"></button> <button class="downvote"><img src="icons/downvote.png"></button> <button class="like"><img src="icons/like.png"><button class="comment-btn"><img src="icons/comment.png" alt="comment"></button>
  </div>
@@ -52,7 +40,7 @@ const markupTxt = `
       <h2>${article.votes}</h2>
     </div>
     <div class="article_media">
-      <p class="article">${article.text}</p>
+      <p class="article">${article.article}</p>
     </div>
     <div class="buttons"> <button class="upvote"><img src="icons/upvote.png"></button> <button class="downvote"><img src="icons/downvote.png"></button> <button class="like"><img src="icons/like.png"><button class="comment-btn"><img src="icons/comment.png" alt="comment"></button>
  </div>
@@ -76,8 +64,102 @@ const markupComment = `
 	</footer>
   </article>
 `;
+*/
 
+const markupImg = `
+  <!-- MEDIA ARTICLE -->
+  <article>
+    <div class="article_top">
+      <header>
+		<div class="profile_border">
+			<div class="cropper">
+				<img src="${article.user}" class="profile">
+			</div>
+		</div>        
+		<h1>${article.title}</h1>
+      </header>
+      <h2>VOTES</h2>
+    </div>
+    <div class="article_media"> <img src="${article.article}" class="article">
+    </div>
+    <div class="buttons"> <button class="upvote"><img src="icons/upvote.png"></button> <button class="downvote"><img src="icons/downvote.png"></button> <button class="like"><img src="icons/like.png"><button class="comment-btn"><img src="icons/comment.png" alt="comment"></button>
+ </div>
+  </article>
+`;
 
-document.querySelector("main").innerHTML += markupTxt;
-document.querySelector("main").innerHTML += markupImg;
+const markupTxt = `
+<!-- TEXT ARTICLE -->
+ <article>
+    <div class="article_top">
+      <header>
+		<div class="profile_border">
+			<div class="cropper">
+				<img src="${article.user}" class="profile">
+			</div>
+		</div>
+		<h1>${article.title}</h1>
+      </header>
+      <h2>VOTES</h2>
+    </div>
+    <div class="article_media">
+      <p class="article">${article.article}</p>
+    </div>
+    <div class="buttons"> <button class="upvote"><img src="icons/upvote.png"></button> <button class="downvote"><img src="icons/downvote.png"></button> <button class="like"><img src="icons/like.png"><button class="comment-btn"><img src="icons/comment.png" alt="comment"></button>
+ </div>
+  </article>
+`;
+
+const markupComment = `
+<!-- Comment -->
+	<article class="comment">
+    <div class="article_top">
+      <header>
+        <div class="profile_border">
+          <div class="cropper"><img src="${comment.profilePic}" class="profile"></div>
+        </div>
+        <h1 class="username">${comment.user}</h1>
+      </header>
+    </div>
+      <p>${comment.commentText}</p>
+	<footer class="comment-footer">
+		<h2>VOTES</h2>
+	</footer>
+  </article>
+`;
+
+// GET
+const getJson = (responseServlet) => {
+	fetch(responseServlet)
+		.then((response) => {
+			return response.json();
+		})
+		.then((result) => {
+			console.log(result);
+		});
+};
+
+// JSON.parse(getJson(jsonSerlvet));
+
+const markupFeed = (jsonSerlvet) => {
+	
+	const json = getJson(jsonSerlvet);
+	
+	for (var i = 0; i < json.length; i++) {
+
+		article = {
+			user: json[i].user,
+			title: json[i].title,
+			article: json[i].article,
+			hasMedia: json[i].hasMedia
+		};
+
+		if (article.hasMedia === 1) {
+			document.querySelector("main").innerHTML += markupImg;
+		} else {
+			document.querySelector("main").innerHTML += markupTxt;
+		}
+	}
+};
+
+document.onload.markupFeed('http://10.114.34.142:8080/BreakServer/webresources/service/getAllMedia?');
 document.querySelector(".comments-container").innerHTML += markupComment;
