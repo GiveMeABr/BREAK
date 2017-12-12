28c64faacb8fbfec252b60be086fc6a100b76976
Mikael Ahlström <mikael.ahlstrom@metropolia.fi>
2017-12-12 02:16:58 +0200

    anonymus txt

--- BreakServer/web/js/markup.js
+++ BreakServer/web/js/markup.js
@@ -2,48 +2,48 @@
 /*jshint esversion: 6 */
 
 let article = {
-	ID: "",
-	user: "",
-	profilePic: "",
-	title: "",
-	article: "",
-	hasMedia: ""
+    ID: "",
+    user: "",
+    profilePic: "",
+    title: "",
+    article: "",
+    hasMedia: ""
 };
 
 const markupFeed = (jsonServlet) => {
 
-	let json;
+    let json;
 
-	fetch(jsonServlet)
-		.then((response) => {
-			return response.json();
-		})
-		.then((result) => {
+    fetch(jsonServlet)
+        .then((response) => {
+            return response.json();
+        })
+        .then((result) => {
 
-			json = result;
-			console.log("json length: " + json.length);
+            json = result;
+            console.log("json length: " + json.length);
 
-			for (var i = 0; i < json.length; i++) {
+            for (var i = 0; i < json.length; i++) {
 
-				/*
-				article.ID = json[i].ID;
-				article.user = json[i].user;
-				article.title = json[i].title;
-				article.article = json[i].article;
-				article.hasMedia = json[i].hasMedia;
-				*/
-				
-				article.ID = json[i][0];
-				article.user = json[i][1];
-				article.profilePic = json[i][2];
-				article.title = json[i][3];
-				article.article = json[i][4];
-				article.hasMedia = json[i][5];
+                /*
+                article.ID = json[i].ID;
+                article.user = json[i].user;
+                article.title = json[i].title;
+                article.article = json[i].article;
+                article.hasMedia = json[i].hasMedia;
+                */
 
-				console.log(article);
-				console.log(article.hasMedia);
+                article.ID = json[i][0];
+                article.user = json[i][1];
+                article.profilePic = json[i][2];
+                article.title = json[i][3];
+                article.article = json[i][4];
+                article.hasMedia = json[i][5];
 
-				let markupImg = `
+                console.log(article);
+                console.log(article.hasMedia);
+
+                let markupImg = `
   <!-- MEDIA ARTICLE -->
   <article id="postId-${article.ID}">
     <div class="article_top">
@@ -64,16 +64,11 @@ const markupFeed = (jsonServlet) => {
   </article>
 `;
 
-				let markupTxt = `
+                let markupTxt = `
 <!-- TEXT ARTICLE -->
 <article id="postId-${article.ID}">
 	<div class="article_top">
       <header>
-		<div class="profile_border">
-			<div class="cropper">
-				<img src="${article.profilePic}" class="profile">
-			</div>
-		</div>
 		<h1>${article.title}</h1>
       </header>
       <h2>VOTES</h2>
@@ -85,16 +80,16 @@ const markupFeed = (jsonServlet) => {
  </div>
   </article>
 `;
-				// THIS MUST BE WITH == NOT WITH === !!!!!!!!
-				if (article.hasMedia == 1) {
-					console.log("CREATING IMAGE TING");
-					document.querySelector("main").innerHTML += markupImg;
-				} else {
-					console.log("CREATING TEXT TING");
-					document.querySelector("main").innerHTML += markupTxt;
-				}
-			}
-		});
+                // THIS MUST BE WITH == NOT WITH === !!!!!!!!
+                if (article.hasMedia == 1) {
+                    console.log("CREATING IMAGE TING");
+                    document.querySelector("main").innerHTML += markupImg;
+                } else {
+                    console.log("CREATING TEXT TING");
+                    document.querySelector("main").innerHTML += markupTxt;
+                }
+            }
+        });
 };
 
 
